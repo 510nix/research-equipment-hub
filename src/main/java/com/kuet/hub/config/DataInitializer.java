667e079ef92f0b7319d783b2da1976b0690ed395
H.M. Azrof <azrof2107088@stud.kuet.ac.bd>
2026-03-19 01:46:34 +0600

    datainitializer updated

--- src/main/java/com/kuet/hub/config/DataInitializer.java
+++ src/main/java/com/kuet/hub/config/DataInitializer.java
@@ -11,12 +11,14 @@ import com.kuet.hub.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
+@Profile("!test")
 public class DataInitializer implements CommandLineRunner {
 
     private final RoleRepository roleRepository;
